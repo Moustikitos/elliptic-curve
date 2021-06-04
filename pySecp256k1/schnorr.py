1cabfec93b1db1dfda3e022c8552a8c4f7ce6bd2
Moustikitos <moustikitos@gmail.com>
2021-06-04 14:27:54 +0200

    suspended schnorr sign and verify

--- pySecp256k1/schnorr.py
+++ pySecp256k1/schnorr.py
@@ -51,6 +51,8 @@ hub.com/bcoin-org/bcrypto/blob/v4.1.0/lib/js/schnorr.js>`_ spec.
     Returns:
         :class:`bytes`: RAW signature
     """
+    # print(msg.hex())
+    # print(seckey0.hex())
     if len(msg) != 32:
         raise ValueError('The message must be a 32-byte array.')
 
@@ -65,19 +67,22 @@ hub.com/bcoin-org/bcrypto/blob/v4.1.0/lib/js/schnorr.js>`_ spec.
         raise RuntimeError(
             'Failure. This happens only with negligible probability.'
         )
+    # print("[P]>>> hash = %s" % hash_sha256(seckey0 + msg).hex())
+    # print("[P]>>> k = %x" % k0)
 
     R = G * k0
     Rraw = bytes_from_int(R.x)
     e = int_from_bytes(
         hash_sha256(Rraw + encoded_from_point(G*seckey) + msg)
     ) % n
+    # print("[P]>>> e = %x" % e)
 
-    seckey %= n
-    k0 %= n
+    # seckey %= n
+    # k0 %= n
     k = n - k0 if not is_quad(R.y) else k0
 
     s = (k + e * seckey) % n
-    s %= n
+    # s %= n
 
     return Rraw + bytes_from_int(s)
 
@@ -95,6 +100,7 @@ norr <https://github.com/bcoin-org/bcrypto/blob/v4.1.0/lib/js/schnorr.js>`_
     Returns:
         :class:`bool`: True if match
     """
+    print(">>>", msg)
     if len(msg) != 32:
         raise ValueError('The message must be a 32-byte array.')
     if len(sig) != 64:
@@ -113,77 +119,77 @@ norr <https://github.com/bcoin-org/bcrypto/blob/v4.1.0/lib/js/schnorr.js>`_
     return True
 
 
-def sign(msg, seckey0):
-    """
-    Generate message signature according to `BIP schnorr <https://github.com/b\
-itcoin/bips/blob/master/bip-0340.mediawiki>`_ spec.
+# def sign(msg, seckey0):
+#     """
+#     Generate message signature according to `BIP schnorr <https://github.com/b\
+# itcoin/bips/blob/master/bip-0340.mediawiki>`_ spec.
 
-    Args:
-        msg (:class:`bytes`): sha256 message-hash
-        seckey0 (:class:`bytes`): private key
-    Returns:
-        :class:`bytes`: RAW signature
-    """
-    if len(msg) != 32:
-        raise ValueError('The message must be a 32-byte array.')
+#     Args:
+#         msg (:class:`bytes`): sha256 message-hash
+#         seckey0 (:class:`bytes`): private key
+#     Returns:
+#         :class:`bytes`: RAW signature
+#     """
+#     if len(msg) != 32:
+#         raise ValueError('The message must be a 32-byte array.')
 
-    seckey0 = int_from_bytes(seckey0)
-    if not (1 <= seckey0 <= n - 1):
-        raise ValueError(
-            'The secret key must be an integer in the range 1..n-1.'
-        )
+#     seckey0 = int_from_bytes(seckey0)
+#     if not (1 <= seckey0 <= n - 1):
+#         raise ValueError(
+#             'The secret key must be an integer in the range 1..n-1.'
+#         )
 
-    P = G*seckey0
-    seckey = seckey0 if is_quad(P.y) else n - seckey0
+#     P = G*seckey0
+#     seckey = seckey0 if is_quad(P.y) else n - seckey0
 
-    k0 = int_from_bytes(
-        tagged_hash("BIPSchnorrDerive", bytes_from_int(seckey) + msg)
-    ) % n
-    if k0 == 0:
-        raise RuntimeError(
-            'Failure. This happens only with negligible probability.'
-        )
+#     k0 = int_from_bytes(
+#         tagged_hash("BIPSchnorrDerive", bytes_from_int(seckey) + msg)
+#     ) % n
+#     if k0 == 0:
+#         raise RuntimeError(
+#             'Failure. This happens only with negligible probability.'
+#         )
 
-    R = G*k0
-    k = n - k0 if not is_quad(R.y) else k0
-    r = bytes_from_point(R)
-    e = int_from_bytes(
-        tagged_hash("BIPSchnorr", r + bytes_from_point(P) + msg)
-    ) % n
+#     R = G*k0
+#     k = n - k0 if not is_quad(R.y) else k0
+#     r = bytes_from_point(R)
+#     e = int_from_bytes(
+#         tagged_hash("BIPSchnorr", r + bytes_from_point(P) + msg)
+#     ) % n
 
-    return r + bytes_from_int((k + e * seckey) % n)
+#     return r + bytes_from_int((k + e * seckey) % n)
 
 
-def verify(msg, pubkey, sig):
-    """
-    Check if public key match message signature according to `BIP schnorr <htt\
-ps://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki>`_ spec.
+# def verify(msg, pubkey, sig):
+#     """
+#     Check if public key match message signature according to `BIP schnorr <htt\
+# ps://github.com/bitcoin/bips/blob/master/bip-0340.mediawiki>`_ spec.
 
-    Args:
-        msg (:class:`bytes`): sha256 message-hash
-        pubkey (:class:`bytes`): encoded public key
-        sig (:class:`bytes`): signature
-    Returns:
-        :class:`bool`: True if match
-    """
-    if len(msg) != 32:
-        raise ValueError('The message must be a 32-byte array.')
-    if len(pubkey) != 32:
-        raise ValueError('The public key must be a 32-byte array.')
-    if len(sig) != 64:
-        raise ValueError('The signature must be a 64-byte array.')
+#     Args:
+#         msg (:class:`bytes`): sha256 message-hash
+#         pubkey (:class:`bytes`): encoded public key
+#         sig (:class:`bytes`): signature
+#     Returns:
+#         :class:`bool`: True if match
+#     """
+#     if len(msg) != 32:
+#         raise ValueError('The message must be a 32-byte array.')
+#     if len(pubkey) != 32:
+#         raise ValueError('The public key must be a 32-byte array.')
+#     if len(sig) != 64:
+#         raise ValueError('The signature must be a 64-byte array.')
 
-    P = point_from_bytes(pubkey)
-    if (P is None):
-        return False
+#     P = point_from_bytes(pubkey)
+#     if (P is None):
+#         return False
 
-    r, s = int_from_bytes(sig[:32]), int_from_bytes(sig[32:])
-    if (r >= p or s >= n):
-        return False
+#     r, s = int_from_bytes(sig[:32]), int_from_bytes(sig[32:])
+#     if (r >= p or s >= n):
+#         return False
 
-    e = int_from_bytes(tagged_hash("BIPSchnorr", sig[0:32] + pubkey + msg)) % n
-    R = Point(*(G*s + point_mul(P, n-e)))  # P*(n-e) does not work...
-    if R is None or not is_quad(R.y) or R.x != r:
-        return False
+#     e = int_from_bytes(tagged_hash("BIPSchnorr", sig[0:32] + pubkey + msg)) % n
+#     R = Point(*(G*s + point_mul(P, n-e)))  # P*(n-e) does not work...
+#     if R is None or not is_quad(R.y) or R.x != r:
+#         return False
 
-    return True
+#     return True
